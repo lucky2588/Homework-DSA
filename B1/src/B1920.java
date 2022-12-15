1213de998671672e5c2f649b22a7b9f9a023b82e
lucky2588 <duwcsthang2001@gmail.com>
2022-12-15 16:08:16 +0700

    B1051

--- B1/src/B1920.java
+++ B1/src/B1920.java
@@ -1,10 +1,24 @@
 // 1920. Build Array from Permutation
 public class B1920 {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+    }
     public int[] buildArray(int[] nums) {
        int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length ; i++) {
             ans[i] = nums[nums[i]];
         }
         return ans;
+    }
+    public static void sortArray(int[] nums1 , int m ,int[] nums2 , int n  ){
+        for (int i = 0; i < nums1.length ; i++) {
+            for (int j = 0; j < nums2.length ; j++) {
+                if(nums1[i]== 0){
+
+                }
+            }
+
+        }
+
     }
 }
