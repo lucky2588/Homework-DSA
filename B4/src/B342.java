5fce6af715a0a5f880be342980a0255e34ab9f85
lucky2588 <duwcsthang2001@gmail.com>
2022-12-17 14:00:40 +0700

    Create B342.java

---
+++ B4/src/B342.java
@@ -0,0 +1,9 @@
+public class B342 {
+
+    public boolean isPowerOfFour(int n) {
+        if(n == 0) return false;
+        if(n == 1) return true;
+        if(n % 4 != 0 )return false;
+        return isPowerOfFour(n/4);
+    }
+}
