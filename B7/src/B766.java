1bb4fba97ea4d9ffd7f518ec7e36dcb603376cc9
lucky2588 <duwcsthang2001@gmail.com>
2022-12-29 01:36:01 +0700

    Homework

--- B7/src/B766.java
+++ B7/src/B766.java
@@ -7,8 +7,9 @@ public class B766 {
     public boolean isToeplitzMatrix(int[][] matrix) {
         for (int i = 1; i <matrix.length-1; i++){ // Vòng đầu tiền phải chạy N lần ( hết chiều dài của mảng )
             for (int j = 1; j <matrix[i].length-1; j++) { //mỗi vòng N  phải chạy hết chiều rộng của mảng M lần
-                if(matrix[i][j] != matrix[i-1][j-1] ) // trong mỗi lần chạy chiều rộng , ta qiu định các phép tính gắn vs + - quy là 1 mỗi 1 (M+1)
-                    return false;
+                if(matrix[i][j] != matrix[i-1][j-1] )
+                    // trong mỗi lần chiều chiều rộng , ta phải thực hiện : M lần điều kiện , M lần tăng J , M lần trong ĐK If
+                    return false; // =>> 3M +1 = M+1
                 // Tổng chạy 2 vòng lòng : N X (M+1) = NxM + N == NxM =>> O(n^2);
             }
         }
