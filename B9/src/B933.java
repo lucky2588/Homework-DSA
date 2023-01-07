09392aaefee49451d2ee1772612e6206da558c0d
lucky2588 <duwcsthang2001@gmail.com>
2023-01-07 17:26:13 +0700

    Homework

---
+++ B9/src/B933.java
@@ -0,0 +1,20 @@
+public class B933 {
+
+
+    class RecentCounter{
+
+        public RecentCounter(){
+
+        }
+
+        public int ping(int t) {
+
+
+        }
+    }
+    public int getSize{
+        return()
+    }
+
+
+}
