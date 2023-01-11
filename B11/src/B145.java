b02fa3a39ec3ee6e43a353dcd911e9b9a6d2cb53
lucky2588 <duwcsthang2001@gmail.com>
2023-01-12 05:14:27 +0700

    homework

---
+++ B11/src/B145.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class B145 {
+    LinkedList<Integer> res = new LinkedList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postOder(root);
+        return res;
+    }
+
+    // Đệ quy
+    public int postOder(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        postOder(root.left);
+        postOder(root.right);
+        res.add(root.val);
+        return 0;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
